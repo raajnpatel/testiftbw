a0daabc7d9bfc846cdadba12041f063fef840e4e
rndpatel@gmail.com <rndpatel@gmail.com>
2019-10-21 14:40:20 -0700

    initial commit

---
+++ src/main/java/local/testifybw/testiftbw/TestiftbwApplication.java
@@ -0,0 +1,15 @@
+package local.testifybw.testiftbw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestiftbwApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(TestiftbwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/local/testifybw/testiftbw/TestiftbwApplicationTests.java
@@ -0,0 +1,15 @@
+package local.testifybw.testiftbw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestiftbwApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
